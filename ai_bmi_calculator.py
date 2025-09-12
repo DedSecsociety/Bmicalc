ca5a45c6333c81350358d38985cca3fb70d8c112
DedSecsociety <souvikdas92@gmail.com>
2025-09-12 18:55:25 +0530

    Add files via upload
    V2

--- ai_bmi_calculator.py
+++ ai_bmi_calculator.py
@@ -11,12 +11,17 @@ name = st.text_input("Enter your name:")
 wt = st.number_input("Enter your weight in kg:")
 ht = st.slider("Enter your height in cm:",50,250)
 age=st.number_input("Enter your age:")
-gender=st.text_input("Enter your gender:")
-bmi = round(wt/(ht/100)**2,2)
+gender = st.selectbox("Select your gender:", ["", "Male", "Female"])
+if st.button("Calculate BMI & Get Advice"):
+ if name and wt > 0 and ht > 0 and age > 0 and gender:
+  bmi = round(wt/(ht/100)**2,2)
 
-st.write(f"{name}, your bmi is : {bmi}")
+  st.success(f" {name}, your **BMI is: {bmi}** ")
 
-prompt = f"Act like an expert nutritionist, comment on the bmi with the following data: height as{ht}, weight as{wt}, age as{age}, gender as{gender} and bmi as{bmi}"
-response = model.generate_content(prompt)
-print(response.text)
-st.markdown(response.text)
+  prompt = f"Act like an expert nutritionist, comment on the bmi with the following data: height as{ht}, weight as{wt}, age as{age}, gender as{gender} and   bmi as{bmi}"
+  response = model.generate_content(prompt)
+  print(response.text)
+
+  st.markdown(response.text)
+else:
+    st.warning("⚠️ Please fill in **all details** before submitting.")
